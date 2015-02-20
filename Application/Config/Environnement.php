c741a3c7db8542325ff2c4b1200302b53b1c4333
camael24 <thehawk@hoa-project.net>
2015-02-20 14:03:06 +0100

    Try autoconf

--- Application/Config/Environnement.php
+++ Application/Config/Environnement.php
@@ -3,11 +3,29 @@
  * @var \Sohoa\Framework\Environnement $this;
  */
 $app = resolve('hoa://Application/');
-$db  = realpath($app.'/../../maestria/www/');
+
+// Autoconfiguration du PATH
+$paths = [
+	$app .'/../../maestria-web/', // Bureau
+	$app .'/../../www/',          // Maison
+];
+
+$db = false;
+foreach ($paths as $key => $value) {
+	if($db === false) {
+		if(false !== $db = realpath($value) ) {
+			break
+		}
+	}
+}
 
 if ($db === false) {
     throw new \Exception("You need to config the Maestria config dir in ".__FILE__, 1);
 }
+
+if(file_exists($db.'/Application/Database/Maestria.db') === false)
+	throw new \Exception('You need to install/generate Metaphsik/Maestria-web repository before use it');
+
 
 \Hoa\Database\Dal::initializeParameters(array(
     'connection.list.default.dal' => Hoa\Database\Dal::PDO,
