80fdc08069b0645dcc9a220e956a665db60fe16a
camael24 <thehawk@hoa-project.net>
2015-02-20 18:38:24 +0100

    Add API response

---
+++ Application/Controller/Generic.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Application\Controller;
+
+use Sohoa\Framework\Kit;
+
+class Generic extends Kit
+{
+    public static $ok = 200;
+    public static $error = 500;
+
+    private $_message = ['status' => 200, 'error' => [], 'log' => [], 'data' => null];
+
+    public function error($message, $data = [])
+    {
+        $this->status(static::$error);
+        $this->_message['error'][] = vsprintf($message, $data);
+    }
+
+    public function log($message, $data = [])
+    {
+        $this->_message['log'][] = vsprintf($message, $data);
+    }
+
+    public function setData($message, $data = [])
+    {
+        $this->_message['data'] = vsprintf($message, $data);
+    }
+
+    public function status($status)
+    {
+        if (isset($this->_message['status']) === true) {
+            if ($this->_message['status'] !== static::$error) {
+                $this->_message['status'] = $status;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function ok($message = 'ok', $data = [])
+    {
+        if ($this->status(static::$ok) === true) {
+            $this->setData($message, $data);
+        }
+    }
+
+    public function nok($message = 'nok', $data = [])
+    {
+        if (isset($this->_message['status']) === true) {
+            if ($this->_message['status'] === static::$error) {
+                $this->setData($message, $data);
+            }
+        }
+    }
+
+    public function getApiJson()
+    {
+        return json_encode($this->_message);
+    }
+}
